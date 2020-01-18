123eb1f8558f803eb5cacfdd5fd4875c2b9aaded
MLakowske <mitch.lakowske@gmail.com>
2020-01-18 10:05:05 -0600

    new

---
+++ src/main/java/frc/robot/commands/SetShooterVelocity.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+
+public class SetShooterVelocity extends CommandBase {
+  /**
+   * Creates a new SetShooterVelocity.
+   */
+  private double setpoint;
+  
+
+  public SetShooterVelocity() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    
+
+    addRequirements(RobotContainer.shooter_subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    setpoint = SmartDashboard.getNumber("SetPoint",0);
+    SmartDashboard.putNumber("SetPoint",setpoint);
+    RobotContainer.shooter_subsystem.setVelocity(setpoint);
+   
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    RobotContainer.shooter_subsystem.UpdateRPMs();
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.shooter_subsystem.setVelocity(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,132 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+  
+  private CANSparkMax bot_shooter = new CANSparkMax(4, MotorType.kBrushless);
+  private CANSparkMax top_shooter = new CANSparkMax(5, MotorType.kBrushless);
+  private CANEncoder  bot_shooter_enc = bot_shooter.getEncoder();
+  private CANEncoder top_shooter_enc = top_shooter.getEncoder();
+  public double kP = .0001;
+  public double kI = .0000005;
+  public double kD = 0;
+  public double kIz = 0;
+  public double kFF = 0;
+  public double maxOutput = 1;
+  public double minOutput = -1;
+  public double maxRPM = 5000;
+  public double bot_top_ratio = 1;
+
+  private CANPIDController bot_shooter_PID = bot_shooter.getPIDController();
+  private CANPIDController top_shooter_PID = top_shooter.getPIDController();
+
+
+
+  public Shooter() {
+    bot_shooter.restoreFactoryDefaults();
+
+    bot_shooter_PID.setP(kP);
+    bot_shooter_PID.setI(kI);
+    bot_shooter_PID.setD(kD);
+    bot_shooter_PID.setIZone(kIz);
+    bot_shooter_PID.setFF(kFF);
+    bot_shooter_PID.setOutputRange(minOutput, maxOutput);
+
+    top_shooter_PID.setP(kP);
+    top_shooter_PID.setI(kI);
+    top_shooter_PID.setD(kD);
+    top_shooter_PID.setIZone(kIz);
+    top_shooter_PID.setFF(kFF);
+    top_shooter_PID.setOutputRange(minOutput, maxOutput);
+    
+    SmartDashboard.putNumber("PGain", kP);
+    SmartDashboard.putNumber("IGain", kI);
+    SmartDashboard.putNumber("DGain", kD);
+    SmartDashboard.putNumber("IZone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", maxOutput);
+    SmartDashboard.putNumber("Min Output", minOutput);
+    SmartDashboard.putNumber("Ratio", bot_top_ratio);
+
+  }
+
+  public void setVelocity(double setpoint){
+    
+    
+    bot_top_ratio = SmartDashboard.getNumber("Ratio",1);
+    updatePIDvariables();
+    bot_shooter_PID.setReference(-setpoint, ControlType.kVelocity);
+    top_shooter_PID.setReference(setpoint*bot_top_ratio*1, ControlType.kVelocity);
+    
+    
+  }
+
+
+
+  public void updatePIDvariables(){
+    double p = SmartDashboard.getNumber("PGain", 0);
+    double i = SmartDashboard.getNumber("IGain", 0);
+    double d = SmartDashboard.getNumber("DGain", 0);
+    double iz = SmartDashboard.getNumber("IZone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+
+
+    if((p != kP)) { bot_shooter_PID.setP(p); top_shooter_PID.setP(p); kP = p; }
+    if((i != kI)) { bot_shooter_PID.setI(i); top_shooter_PID.setI(i); kI = i; }
+    if((d != kD)) { bot_shooter_PID.setD(d); top_shooter_PID.setD(d); kD = d; }
+    if((iz != kIz)) { bot_shooter_PID.setIZone(iz); top_shooter_PID.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { bot_shooter_PID.setFF(ff); top_shooter_PID.setFF(ff);kFF = ff; }
+    if((max != maxOutput) || (min != minOutput)) { 
+      bot_shooter_PID.setOutputRange(min, max); 
+      top_shooter_PID.setOutputRange(min, max); 
+      minOutput = min; maxOutput = max; 
+    }
+    SmartDashboard.putNumber("PGain", kP);
+    SmartDashboard.putNumber("IGain", kI);
+    SmartDashboard.putNumber("DGain", kD);
+    SmartDashboard.putNumber("IZone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", maxOutput);
+    SmartDashboard.putNumber("Min Output", minOutput);
+    SmartDashboard.putNumber("Ratio", bot_top_ratio);
+   
+  }
+  public void motorIdle(){
+    
+  }
+
+
+  public void UpdateRPMs() {
+    
+    SmartDashboard.putNumber("Bot Actual RPM", -bot_shooter_enc.getVelocity());
+    SmartDashboard.putNumber("Top Actual RPM", top_shooter_enc.getVelocity());
+  }
+
+
+  @Override
+  public void periodic() {
+
+    SmartDashboard.putNumber("Bot Actual RPM", -bot_shooter_enc.getVelocity());
+    SmartDashboard.putNumber("Top Actual RPM", top_shooter_enc.getVelocity());
+    // This method will be called once per scheduler run
+    
+
+  }
+}
