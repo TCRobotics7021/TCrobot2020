71c28761b252d90a8c776880dce25c4e7095f83c
Robotics <FirstRobotics@ashleyfurniture.com>
2020-01-28 18:04:33 -0600

    added accumilator delay

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,11 +20,8 @@ import frc.robot.commands.Manual_Turret;
 import frc.robot.commands.SetShooterVelocity;
 import frc.robot.commands.Shoot_Energy;
 import frc.robot.commands.Shoot_Energy_At_Target;
-<<<<<<< HEAD
 import frc.robot.commands.TankDrive;
-=======
 import frc.robot.commands.Toggle_Auto_Aim;
->>>>>>> 2e175a9f7362fc7a9793daba337642a31bcaa9bd
 import frc.robot.subsystems.Accumulator;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.ExampleSubsystem;
@@ -90,11 +87,8 @@ public class RobotContainer {
     new JoystickButton(JoyR, 2).whileHeld(new Manual_Turret(.5));
     new JoystickButton(JoyL, 2).whileHeld(new Manual_Turret(-.5));
     new JoystickButton(JoyL, 3).whileHeld(new Aim_At_Target());
-<<<<<<< HEAD
     new JoystickButton(JoyR, 7).whenPressed(new DriveInvertedToggle());
-=======
     new JoystickButton(JoyR, 14).whenPressed(new Toggle_Auto_Aim());
->>>>>>> 2e175a9f7362fc7a9793daba337642a31bcaa9bd
   }
 
 
