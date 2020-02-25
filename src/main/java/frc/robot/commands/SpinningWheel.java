201666eb2b780f5e0c4a67bed356393338f4730e
Robotics <FirstRobotics@ashleyfurniture.com>
2020-02-24 18:08:37 -0600

    intake, colorwheel, autotomonous

--- src/main/java/frc/robot/commands/SpinningWheel.java
+++ src/main/java/frc/robot/commands/SpinningWheel.java
@@ -26,53 +26,59 @@ public class SpinningWheel extends CommandBase {
   int Target;
   String PreviousColor;
   String CurrentColor;
-
+  String NextColor;
   boolean Finished;
 
   public SpinningWheel(int Target){
-
     addRequirements(RobotContainer.ColorWheel_subsystem);
     this.Target = Target;
     // Use addRequirements() here to declare subsystem dependencies.
   }
-
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
    Counter = 0;
    PreviousColor = RobotContainer.ColorWheel_subsystem.getCurrentColor();
-   RobotContainer.ColorWheel_subsystem.setWheelSpeed(0.7);
+   RobotContainer.ColorWheel_subsystem.setWheelSpeed(0.4);
    ColorDelay.start();
    Finished = false;
   }  
-
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     if(ColorDelay.get()>0.2){
       CurrentColor = RobotContainer.ColorWheel_subsystem.getCurrentColor();
-    if (!CurrentColor.equals(PreviousColor)) {
+    if (CurrentColor.equals(NextColor)) {
       Counter += 1;
       SmartDashboard.putNumber("Color Counter", Counter);
       SmartDashboard.putString("Current Color", CurrentColor);
-      PreviousColor = RobotContainer.ColorWheel_subsystem.getCurrentColor(); 
-     
+      PreviousColor = RobotContainer.ColorWheel_subsystem.getCurrentColor();   
       }
+    if(CurrentColor.equals("Green")) {
+      NextColor = "Blue";
+    }
+    if(CurrentColor.equals("Blue")) {
+      NextColor = "Yellow";
+    }
+    if(CurrentColor.equals("Yellow")) {
+      NextColor = "Red";
+    }
+    if(CurrentColor.equals("Red")) {
+      NextColor = "Green";
+    }
       ColorDelay.reset();
       ColorDelay.start();
+    
     }
     if (Counter >= Target) {
       Finished = true;
     }
-    
   }
-
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     RobotContainer.ColorWheel_subsystem.setWheelSpeed(0);
   }
-
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
